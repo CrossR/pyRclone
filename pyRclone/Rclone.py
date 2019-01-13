310177738ce0600d2495adeb1951acefbb41e6f3
Ryan C <r.cross@lancaster.ac.uk>
2019-01-13 13:10:27 +0000

    Add basic method to run an rclone command.

--- pyRclone/Rclone.py
+++ pyRclone/Rclone.py
@@ -1,8 +1,17 @@
 import logging
 import subprocess
 from typing import List, Optional
+from dataclasses import dataclass
 
 from .RcloneConfig import RcloneConfig
+
+
+@dataclass
+class RcloneOutput:
+    return_code: int
+    output: List[str]
+    error: List[str]
+
 
 class Rclone:
     """Rclone
@@ -17,3 +26,39 @@ class Rclone:
             self.config: RcloneConfig = RcloneConfig.get_default_config()
         else:
             self.config: RcloneConfig = config
+
+    def _execute(self, command_to_run: List[str]) -> RcloneOutput:
+        """_execute
+
+        A helper function to run a given rclone command, and return the output.
+
+        The command is expected to be given as a list of strings, ie
+        the command "rclone lsd dropbox:" would be:
+            ["rclone", "lsd", "dropbox:"]
+        """
+        self.logger.debug(f"Running: {command_to_run}")
+
+        try:
+            with subprocess.Popen(
+                command_to_run, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+            ) as rclone_process:
+                output: str
+                error: bytes
+                (output, error) = rclone_process.communicate()
+
+                self.logger.debug(f"Command returned {output}")
+
+                if error:
+                    self.logger.warning(error.decode("utf-8").replace("\\n", "\n"))
+
+                return RcloneOutput(
+                    return_code=rclone_process.returncode,
+                    output=output.splitlines(),
+                    error=error.decode("utf-8").splitlines(),
+                )
+        except FileNotFoundError as e:
+            self.logger.exception(f"Can't find rclone executable. {e}")
+            return RcloneOutput(return_code=-2, output=[""], error=[""])
+        except Exception as e:
+            self.logger.exception(f"Exception running {command_to_run}. Exception: {e}")
+            return RcloneOutput(return_code=-2, output=[""], error=[""])
--- tests/debug_parser.py
+++ tests/debug_parser.py
@@ -1,3 +1,7 @@
 from pyRclone import Rclone
 
 DEBUG_PARSER: Rclone = Rclone()
+COMMAND_TO_RUN = ["rclone", "lsd", "drive:"]
+TEST_OUTPUT = DEBUG_PARSER._execute(COMMAND_TO_RUN)
+
+print(TEST_OUTPUT)
